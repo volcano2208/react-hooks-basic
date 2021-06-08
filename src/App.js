beaed481699dc3ee8fa79a186c2dbdfb018b480d
Thanh Pham Tien <tienthanh@inspectorio.com>
2021-06-08 16:50:26 +0700

    color box change color function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
+
+  // Array destructuring syntax 
+
+
   return (
     <div className="app">
       <h1>Welcome to React Hooks</h1>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import "./ColorBox.scss";
+ColorBox.propTypes = {};
+
+function getRandomColor() {
+  const COLOR_LIST = ["deeppink", "green", "yellow", "black"];
+  const randomIndex = Math.trunc(Math.random() * 4);
+  return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+  const [color, setColor] = useState(() => {
+    const initColor = localStorage.getItem("color_box") || "deeppink";
+    return initColor;
+  });
+
+  function handleBoxClick() {
+    const newColor = getRandomColor();
+    setColor(newColor);
+    localStorage.setItem("box_color", newColor);
+  }
+  return (
+    <div
+      className="color-box"
+      style={{backgroundColor: color}}
+      onClick={handleBoxClick}
+    ></div>
+  );
+}
+
+export default ColorBox;
